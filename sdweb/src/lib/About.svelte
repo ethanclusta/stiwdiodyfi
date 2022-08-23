fd73933f1cf25be3db413b6342d8a79fb92a9ebd
Ethan Corcoran <ethan@clusta.org>
2022-08-23 13:06:43 +0100

    update layout / content of contact and partners sections

--- sdweb/src/lib/About.svelte
+++ sdweb/src/lib/About.svelte
@@ -1,27 +1,51 @@
-<div class="px-5">
-	<div class="bg-gray-100 p-3 w-full lg:w-full flex flex-col gap-y-3">
-		<h1 class="text-[#D32D7D] font-bold text-5xl">About</h1>
-		<p>Stiwdio Dyfi CIC is a regeneration project established on the 12th January 2022.</p>
-		<p>We are set up as a community interest company limited by guarantee.</p>
+<div class="px-5 flex flex-col lg:flex-row gap-3">
+	<div class="bg-gray-100 p-3 w-full lg:w-1/2 flex flex-col gap-y-3 text-sm">
+		<h1 class="text-[#D32D7D] font-bold text-3xl">About</h1>
+		<p class="text-lg font-semibold">
+			Stiwdio Dyfi represents the first small steps in a large
+			ambition: to instigate and sustain a film and media industry in the Dyfi Biosphere.
+		</p>
 		<p>
-			We are operating within the UNESCO Dyfi Biosphere area in Mid-West Wales and have a studio
-			facility at Y Plas, Machynlleth.
+			Stiwdio Dyfi CIC is a regeneration project operating within the UNESCO Dyfi Biosphere area in
+			Mid-West Wales, with a studio facility at Y Plas, Machynlleth.
 		</p>
 		<p>
 			We have an ethos of inclusion, accessibility and sustainability outlined in our CIC36
 			manifesto.
 		</p>
 		<p>
-			We have set up to give the local economy a boost through providing cultural spaces and outlets
+			Stiwdio Dyfi was set up to give the local economy a boost through providing cultural spaces and outlets
 			that complement existing infrastructure and enterprise.
 		</p>
 		<p>
 			Our aim is to initiate and facilitate creative spaces and outlets to enhance creative economy
 			opportunity in the Dyfi Biosphere.
 		</p>
-		<p>Stiwdio Dyfi has received funding and support from the following</p>
 		<p>
 			William Tremlett is the Project Manager and you can contact him here: stiwdiodyfi@gmail.com
 		</p>
+	</div>
+	<div class="w-full lg:w-1/2 flex flex-col gap-y-3 text-sm">
+		<div class="bg-gray-100 p-3 w-full flex flex-col gap-y-3">
+			<h1 class="text-[#D32D7D] font-bold text-3xl">Connect with us</h1>
+			<p>See our launch publication here.</p>
+			<p>
+				Find our stall at Machynlleth Market every Wednesday to pick up a flyer for the next 7 days.
+				Hopefully we will see you there!
+			</p>
+			<p>Sign up for weekly updates here:</p>
+			<div class="flex">
+				<input
+					type="text"
+					placeholder="Your email"
+					class="p-2 border w-2/3 lg:w-1/2 outline-none rounded-l-md border-[#D32D7D]"
+				/>
+				<button class="bg-[#D32D7D] text-white rounded-r-md p-2">Subscribe</button>
+			</div>
+		</div>
+		<div class="bg-gray-100 p-3 w-full flex flex-col gap-y-3">
+			<h1 class="text-[#D32D7D] font-bold text-3xl">Our partners</h1>
+			<p>Stiwdio Dyfi has received funding and support from the following.</p>
+		</div>
 	</div>
 </div>
