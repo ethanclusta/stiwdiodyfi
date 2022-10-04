cdeddac77ae4c578e93e3713a3060bd860dd1202
Ethan Corcoran <ethan@clusta.org>
2022-10-04 18:38:19 +0100

    add modal for survey

--- sdweb/src/routes/+page.svelte
+++ sdweb/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
 	import About from '../lib/About.svelte';
 	import Cards from '../lib/Cards.svelte';
 	import Hero from '../lib/Hero.svelte';
@@ -12,10 +13,15 @@
 		if (e.target.id === 'hwb' || e.target.id === 'dx3') {
 			content = e.target.id;
 		} else {
-			content = '';
+			content = 'survey';
 		}
 		return;
 	};
+
+	onMount(() => {
+		modal = true;
+		content = 'survey';
+	});
 </script>
 
 <Hero />
@@ -52,7 +58,7 @@
 						Sign up for Hwb Ffasiwn Da's workshops
 					</a>
 				</div>
-			{:else}
+			{:else if content === 'dx3'}
 				<h1
 					class="text-center font-bold text-3xl lg:text-5xl bg-gradient-to-r from-violet-400 to-fuchsia-400 bg-clip-text text-transparent"
 				>
@@ -72,6 +78,26 @@
 					Bandcamp launching this Calan Geaf.
 				</p>
 				<p>Website coming soon...</p>
+			{:else}
+			<div class="bg-[#D32D7D] w-full h-full flex flex-col gap-2 p-6 text-sm lg:text-base text-white">
+				<h1 class="text-center font-bold text-xl lg:text-xl">
+					Be in the draw to win 4 x cinema tickets with complimentary drinks + a £25 gift voucher to
+					Dyfi Wholefoods.
+				</h1>
+				<h1 class="text-center font-bold text-xl lg:text-xl">
+					Bod yn yr cystadleuaeth i ennill 4 x tocunnau sinema hefo diodydd + tocyn anrheg i Dyfi
+					Wholewoods gweth £25.
+				</h1>
+				<p class="text-center ">We'd welcome your thoughts on local arts provision</p>
+				<p class="text-center ">Byddwn yn croesawu eich barn ar meddyliau ar celf lleol</p>
+				<div class="flex place-content-center">
+					<a
+						href="https://us14.list-manage.com/survey?u=c288709774c96731a04d21fa6&id=5a087b5638&attribution=false"
+						target="_blank"
+						class="p-3 bg-zinc-100 text-black font-bold">Complete our survey</a
+					>
+				</div>
+			</div>
 			{/if}
 		</div>
 	</div>
