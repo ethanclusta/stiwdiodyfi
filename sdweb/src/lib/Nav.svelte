0efd7e6a96272f22c32b7d12e96e2e154e258943
Ethan Corcoran <ethan@clusta.org>
2023-03-14 21:17:04 +0000

    update nav item sizes for phone screens, update link to mc contact form

--- sdweb/src/lib/Nav.svelte
+++ sdweb/src/lib/Nav.svelte
@@ -19,7 +19,7 @@
 	<a href="/">
 		<img class="h-16 object-contain" src={Logo} alt="Stiwdio Dyfi" />
 	</a>
-	<div class="flex flex-row gap-x-5 py-4 text-xl md:text-2xl">
+	<div class="flex flex-row gap-x-3 md:gap-x-5 py-4 text-lg md:text-2xl">
 		<div class="relative">
 			<div on:click={toggleDrop} class="cursor-pointer hover:underline">PROJECTS</div>
 			{#if drop}
@@ -27,7 +27,7 @@
 			{/if}
 		</div>
 		<div class="cursor-pointer hover:underline">ABOUT</div>
-		<a href="/contact" class="cursor-pointer hover:underline">CONTACT</a>
+		<a href="https://us14.list-manage.com/contact-form?u=c288709774c96731a04d21fa6&form_id=07bdec94bec06a1ccf2c0fd4575a9ae5" class="cursor-pointer hover:underline">CONTACT</a>
 	</div>
 	<img class="h-16 opacity-0 hidden md:block" src={Logo} alt="Stiwdio Dyfi" />
 </div>
