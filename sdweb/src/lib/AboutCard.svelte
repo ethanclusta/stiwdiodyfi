5cc4718587245e315334513d1e907b2056afcba9
Ethan Corcoran <ethan@clusta.org>
2023-08-28 23:37:55 +0100

    remove underline style from about cards

--- sdweb/src/lib/AboutCard.svelte
+++ sdweb/src/lib/AboutCard.svelte
@@ -2,8 +2,8 @@
     export let text;
     export let navTo;
     let hover = false;
-    const noHover = "bg-black border-2 border-black cursor-pointer text-white font-bold text-2xl text-center w-full lg:w-1/2 p-12";
-    const yesHover = "bg-white border-2 border-black cursor-pointer text-black font-bold text-2xl text-center w-full lg:w-1/2 p-12"
+    const noHover = "bg-black border-2 border-black cursor-pointer text-white font-bold text-2xl text-center no-underline w-full lg:w-1/2 p-12";
+    const yesHover = "bg-white border-2 border-black cursor-pointer text-black font-bold text-2xl text-center no-underline w-full lg:w-1/2 p-12"
 
     const onHover = () => {
         hover = !hover;
