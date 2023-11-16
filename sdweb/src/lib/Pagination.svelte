2ff24c6c4cd999b690bb983b57016eff86611452
Ethan Corcoran <ethan@clusta.org>
2023-11-16 22:41:48 +0000

    add pagination to advisory panel

--- sdweb/src/lib/Pagination.svelte
+++ sdweb/src/lib/Pagination.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let curPage;
 	export let pageCount;
+	import { page } from '$app/stores';
 
 	const pagesLength = (pgCount) => {
 		const resultArray = Array.from({ length: pgCount }, (_, index) => index + 1);
@@ -11,11 +12,13 @@
 
 	const selectedStyle = 'font-bold border border-black p-2';
 	const normalStyle = 'font-regular p-2';
+
+	const curPath = $page.url.pathname;
 </script>
 
 <div class="flex flex-row gap-1 w-full place-content-center">
 	{#each arrayOfPages as page (page)}
-		<a href={`/?page=${page}`} class={page === curPage ? selectedStyle : normalStyle}>
+		<a href={`${curPath}?page=${page}`} class={page === curPage ? selectedStyle : normalStyle}>
 			{page}
         </a>
 	{/each}
