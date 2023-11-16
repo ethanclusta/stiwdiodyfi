a59440dc15867e7ea417718120f867c0560c1b7b
Ethan Corcoran <ethan@clusta.org>
2023-11-16 10:07:19 +0000

    fix tag page titles, ad swn to active projs

--- sdweb/src/routes/[tag]/+page.svelte
+++ sdweb/src/routes/[tag]/+page.svelte
@@ -1,11 +1,15 @@
 <script>
 	import List from '../../lib/List.svelte';
+	import { page } from '$app/stores';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-    const tagData = data.posts[0].tags[0]
-    const titleName = tagData.name;
+	const urlPath = $page.url.pathname.slice(1);
+
+    const tagData = data.posts[0].tags
+	const titleTag = tagData.filter((tag) => tag.slug === urlPath)
+    const titleName = titleTag[0].name
 </script>
 
 <List data={data.posts} title={titleName} />
